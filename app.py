36447e1bb3f568661883211ba8ac95f2ce8a673d
umerhayat93 <95833617+umerhayat93@users.noreply.github.com>
2026-03-16 15:32:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -565,7 +565,3 @@ def notify():
                'body': d.get('body',''),
                'icon': d.get('icon','?')})
     return ok()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
